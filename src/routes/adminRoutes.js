4f61cb1c759b859ef0ca9e61a5fc87a02899c45a
DahobachirGhada <dahobachirghada@gmail.com>
2026-03-29 16:01:38 +0200

    fixed router

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AnalyticsController = require('../controllers/analyticsController');
 const AlertsController = require('../controllers/alertsController');
 const prioritiesController = require('../controllers/prioritiesController');
-const getLatestThree = require('../controllers/aitableController');
+const {getLatestThree} = require('../controllers/aitableController');
 const { protect, restrictTo } = require('../middleware/authMiddleware');
 
 
