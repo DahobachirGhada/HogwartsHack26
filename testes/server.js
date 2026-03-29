05f0e36ec74183d2b0e5b34e097755400b5d911c
DahobachirGhada <dahobachirghada@gmail.com>
2026-03-29 16:13:51 +0200

    n

--- testes/server.js
+++ testes/server.js
@@ -1,5 +1,4 @@
 const app = require('../src/validators/app.js');
-app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
